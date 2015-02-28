413d37c5053535b5540d3632e56931212630d302
Marino Missiroli <m.missiroli@cern.ch>
2015-02-28 17:11:41 +0100

    updated selection module; updated ZprimeSemiLeptonicSelections;

--- include/ZprimeSemiLeptonicSelections.h
+++ include/ZprimeSemiLeptonicSelections.h
@@ -1,134 +1,67 @@
 #pragma once
 
 #include "UHH2/core/include/fwd.h"
+#include "UHH2/core/include/Utils.h"
 #include "UHH2/core/include/Selection.h"
 #include "UHH2/common/include/ReconstructionHypothesisDiscriminators.h"
 
 namespace uhh2 {
     
-/* Select events with at least two jets in which the leading two jets have deltaphi > 2.7 and the third jet pt is
- * below 20% of the average of the leading two jets, where the minimum deltaphi and
- * maximum third jet pt fraction can be changed in the constructor.
- * The jets are assumed to be sorted in pt.
- */
+  class HTlepCut : public Selection {
+   public:
+    explicit HTlepCut(float min_htlep, float max_htlep=infinity);
+    virtual bool passes(const Event &);
 
-  class DijetSelection: public uhh2::Selection {
-  public:
-    DijetSelection(float dphi_min = 2.7f, float third_frac_max = 0.2f);
-    virtual bool passes(const uhh2::Event & event) override;
-  private:
-    float dphi_min, third_frac_max;
+   private:
+    Event::Handle<double> h_htlep_;
+    float min_htlep_, max_htlep_;
   };
 
-class HTlepCut: public Selection {
-public:
-  explicit HTlepCut(Context & ctx, float min_htlep, float max_htlep = -1.);
-  virtual bool passes(const Event &);
+  class METCut : public Selection {
+   public:
+    explicit METCut(float min_met, float max_met=infinity);
+    virtual bool passes(const Event & event) override;
 
-private:
-  Event::Handle<double> h_htlep_;
-  float min_htlep_, max_htlep_;
-};
-
-
-
-class METCut: public Selection {
-public:
-  explicit METCut(double min_met=0., double max_met=-1);
-  virtual bool passes(const Event & event) override;
-
-private:
-  double min_met, max_met;
+   private:
+    float min_met_, max_met_;
   };
 
+  class NJetCut : public Selection {
+   public:
+    explicit NJetCut(int nmin, int nmax=-1, float ptmin=0., float etamax=infinity);
+    virtual bool passes(const Event & event) override;
 
-class NJetCut: public Selection {
-public:
-  /// In case nmax=-1, no cut on the maximum is applied.
-  explicit NJetCut(int nmin, int nmax = -1, double ptmin=0., double etamax = -1);
-  virtual bool passes(const Event & event) override;
-private:
-  int nmin, nmax;
-  double ptmin, etamax;
-};
-    
-class TwoDCut: public Selection {
-public:
-  TwoDCut() {};
-  ~TwoDCut() {};
-  virtual bool passes(const Event & event) override;
-};
-  
+   private:
+    int nmin, nmax;
+    float ptmin, etamax;
+  };
 
+  class TwoDCut : public Selection {
+   public:
+    TwoDCut() {}
+    virtual bool passes(const Event & event) override;
+  };
 
-class CMSTopTagOverlapSelection: public Selection {
- public:
-  explicit CMSTopTagOverlapSelection(double delR_Lep_TopTag = 0.8 , double delR_Jet_TopTag = 1.3);
-  virtual bool passes(const Event & event) override;
+  class TopJetOverlapSelection: public Selection {
+   public:
+    explicit TopJetOverlapSelection(float delR_Lep_TopJet=0.8 , float delR_Jet_TopJet=1.2);
+    virtual bool passes(const Event & event) override;
 
- private:
-  double delR_Lep_TopTag;
-  double delR_Jet_TopTag;
-  std::unique_ptr<Selection> ntopjet_sel;
-  TopJetId topjetid;
-};
+   private:
+    float delR_Lep_TopJet, delR_Jet_TopJet;
+    std::unique_ptr<Selection> ntopjet_sel;
+    TopJetId topjetid;
+  };
 
-
-
-
-
-
-
-
-
-class HypothesisDiscriminatorCut: public Selection {
-public:
-    explicit HypothesisDiscriminatorCut(Context & ctx, double min_discr, double max_discr, const std::string & discriminator_name = "Chi2", const std::string & hyps_name = "HighMassReconstruction");
+  class HypothesisDiscriminatorCut: public Selection {
+   public:
+    explicit HypothesisDiscriminatorCut(Context& ctx, float min_discr, float max_discr, const std::string& discr_name="Chi2", const std::string& hyps_name="HighMassReconstruction");
     virtual bool passes(const Event & event);
 
-private:
-    double m_min_discr_;
-    double m_max_discr_;
+   private:
+    float m_min_discr_, m_max_discr_;
     std::string m_discriminator_name;
     uhh2::Event::Handle<std::vector<ReconstructionHypothesis>> h_hyps;
-    
-
-
-
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  };
 
 }
--- src/ZprimeSelectionModule.cxx
+++ src/ZprimeSelectionModule.cxx
@@ -25,68 +25,72 @@
 #include "UHH2/common/include/ReconstructionHypothesisDiscriminators.h"
 #include "UHH2/common/include/HypothesisHists.h"
 
-
-using namespace std;
 using namespace uhh2;
 
 class ZprimeSelectionModule: public AnalysisModule {
-public:
-    
-    explicit ZprimeSelectionModule(Context & ctx);
-    virtual bool process(Event & event) override;
+ public:
+  explicit ZprimeSelectionModule(Context & ctx);
+  virtual bool process(Event & event) override;
 
-private:    
-  std::unique_ptr<JetCleaner> jetcleaner;
-  std::unique_ptr<MuonCleaner> muoncleaner;
-  std::unique_ptr<ElectronCleaner> electroncleaner;
-  std::unique_ptr<JetLeptonCleaner> jetleptoncleaner;
-  std::unique_ptr<JetCorrector> jetcorrector;
-  JetId jet_kinematic;
-  TopJetId topjetid;
+ private:    
+  std::unique_ptr<MuonCleaner> muo_cleaner;
+  std::unique_ptr<ElectronCleaner> ele_cleaner;
+  std::unique_ptr<JetCorrector> jet_corrector;
+  std::unique_ptr<JetLeptonCleaner> jetlepton_cleaner;
+  std::unique_ptr<JetCleaner> jet_cleaner;
 
   std::vector<std::unique_ptr<AnalysisModule>> recomodules;
   std::vector<std::unique_ptr<AnalysisModule>> pre_modules;
-    
-    // declare the Selections to use. 
-  std::unique_ptr<Selection> nele_sel, nmu_sel, njet_sel, twodcut, htlepcut, metcut, ntopjet_sel, cmstoptagoverlap_sel;
-  // add later: npv_sel, etc.
+
+  // Selection
+  std::unique_ptr<AndSelection> lepN_sel;
+  std::unique_ptr<Selection> pvN_sel, jet1_sel, jet2_sel, htlep_sel, met_sel, twodcut_sel, topjetN_sel, topjetoverlap_sel;
  
-    // store the Hists collection as member variables.
+  // Hists
   std::unique_ptr<Hists> h_hyphists, h_event, h_jet, h_ele, h_mu, h_tau, h_topjet;
   uhh2::Event::Handle<std::vector<ReconstructionHypothesis>> h_hyps;
 };
 
 ZprimeSelectionModule::ZprimeSelectionModule(Context & ctx){
-   
-  jet_kinematic = PtEtaCut(150.0, 2.5);
-  topjetid = CMSTopTag();
 
-    // 1. setup other modules (CommonModules,JetCleaner, etc.):
-  jetcleaner.reset(new JetCleaner(25.0,std::numeric_limits<double>::infinity())); 
-  muoncleaner.reset(new MuonCleaner(AndId<Muon>(MuonIDTight(),PtEtaCut(45.0, 2.1))));
-  electroncleaner.reset(new ElectronCleaner(AndId<Electron>(ElectronID_PHYS14_25ns_medium, PtEtaCut(35.0, 2.5))));
-  jetcorrector.reset(new JetCorrector(JERFiles::PHYS14_L123_MC));
-  jetleptoncleaner.reset(new JetLeptonCleaner(JERFiles::PHYS14_L123_MC));
+  muo_cleaner.reset(new MuonCleaner(AndId<Muon>(MuonIDTight(), PtEtaCut(45., 2.1))));
+  ele_cleaner.reset(new ElectronCleaner(AndId<Electron>(ElectronID_PHYS14_25ns_medium, PtEtaCut(35., 2.5))));
+  jet_corrector.reset(new JetCorrector(JERFiles::PHYS14_L123_MC));
+  jetlepton_cleaner.reset(new JetLeptonCleaner(JERFiles::PHYS14_L123_MC));
+  jet_cleaner.reset(new JetCleaner(25., std::numeric_limits<double>::infinity())); 
   pre_modules.push_back(std::unique_ptr<AnalysisModule>(new HTlepCalculator(ctx)));
-   
-    // 2. set up selections
-    // For Muons only:
-  nele_sel.reset(new NElectronSelection(0,0)); //no electrons
-  nmu_sel.reset(new NMuonSelection(1,1)); // exactly one muon
-  twodcut.reset(new TwoDCut()); 
-  njet_sel.reset(new NJetSelection(1, -1,jet_kinematic)); // at least 2 jets with pt 150 and eta 2.5
-  htlepcut.reset(new HTlepCut(ctx,150,std::numeric_limits<double>::infinity()));
-  metcut.reset(new METCut(20,std::numeric_limits<double>::infinity()));
-  ntopjet_sel.reset(new NTopJetSelection(1,-1,topjetid));
-  cmstoptagoverlap_sel.reset(new CMSTopTagOverlapSelection()); 
 
- //make reconstruction hypotheses
+  bool muon(false), elec(false);
+  const std::string channel(ctx.get("channel", ""));
+  if(channel == "muon") muon = true;
+  else if(channel == "electron") elec = true;
+  else throw std::runtime_error("undefined argument for analysis channel (must be 'muon' or 'electron'): "+channel);
+
+  lepN_sel.reset(new AndSelection(ctx));
+  if(muon){
+    lepN_sel->add<NMuonSelection>("muoN == 1", 1, 1);
+    lepN_sel->add<NElectronSelection>("eleN == 0", 0, 0);
+  }
+  else if(elec){
+    lepN_sel->add<NMuonSelection>("muoN == 0", 0, 0);
+    lepN_sel->add<NElectronSelection>("eleN == 1", 1, 1);
+  }
+
+  jet1_sel.reset(new NJetSelection(2, -1, JetId(PtEtaCut(150., 2.4)))); // at least 2 jets with pt>50 and |eta|<2.4
+  jet2_sel.reset(new NJetSelection(2, -1, JetId(PtEtaCut(50., 2.4)))); // at least 2 jets with pt>50 and |eta|<2.4
+  htlep_sel.reset(new HTlepCut(150., std::numeric_limits<double>::infinity()));
+  met_sel.reset(new METCut(50., std::numeric_limits<double>::infinity()));
+  twodcut_sel.reset(new TwoDCut());
+  topjetN_sel.reset(new NTopJetSelection(1, -1, TopJetId(CMSTopTag())));
+  topjetoverlap_sel.reset(new TopJetOverlapSelection());
+
+  // reconstruction hypotheses
   recomodules.emplace_back(new PrimaryLepton(ctx));
   recomodules.emplace_back(new HighMassTTbarReconstruction(ctx,NeutrinoReconstruction));
   recomodules.emplace_back(new Chi2Discriminator(ctx,"HighMassReconstruction"));
 
-    // 3. Set up Hists classes:
-  h_hyphists.reset(new HypothesisHists(ctx,"Chi2Hists","HighMassReconstruction","Chi2"));
+  // Hists setup
+  h_hyphists.reset(new HypothesisHists(ctx, "Chi2Hists", "HighMassReconstruction", "Chi2"));
   h_hyps = ctx.get_handle<std::vector<ReconstructionHypothesis>>("HighMassReconstruction");
   h_event.reset(new EventHists(ctx, "Event_Sel"));
   h_jet.reset(new JetHists(ctx, "Jets_Sel"));
@@ -96,64 +100,39 @@ ZprimeSelectionModule::ZprimeSelectionModule(Context & ctx){
   h_topjet.reset(new TopJetHists(ctx, "TopJets_Sel"));
 }
 
-bool ZprimeSelectionModule::process(Event & event) {
-    // This is the main procedure, called for each event. Typically,
-    // do some pre-processing by calling the modules' process method
-    // of the modules constructed in the constructor (1).
-    // Then, test whether the event passes some selection and -- if yes --
-    // use it to fill the histograms (2).
-    // Finally, decide whether or not to keep the event in the output (3);
-    // this is controlled by the return value of this method: If it
-    // returns true, the event is kept; if it returns false, the event
-    // is thrown away.
-    //cout << "ZprimeSelectionModule: Starting to process event (runid, eventid) = (" << event.run << ", " << event.event << "); weight = " << event.weight << endl;
-    
+bool ZprimeSelectionModule::process(Event & event){
 
+  muo_cleaner->process(event);
+  ele_cleaner->process(event);
+  jet_corrector->process(event);
+  jetlepton_cleaner->process(event);
+  jet_cleaner->process(event);
 
-    // 1. run all modules other modules.
-  jetcleaner->process(event);
-  muoncleaner->process(event);
-  electroncleaner->process(event);
-  jetcorrector->process(event);
-  jetleptoncleaner->process(event);
+  for(auto & mod : pre_modules) mod->process(event);
 
-  for (auto & mod : pre_modules) {
-  mod->process(event);
-  }
+  bool pass_lepN = lepN_sel->passes(event);
+  bool pass_jet1 = jet1_sel->passes(event);
+  bool pass_jet2 = jet2_sel->passes(event);
+  bool pass_twodcut = twodcut_sel->passes(event);
+  bool pass_htlep = htlep_sel->passes(event);
+  bool pass_met = met_sel->passes(event);
+  bool pass_topjetN = topjetN_sel->passes(event);
+  bool pass_topjetoverlap = topjetoverlap_sel->passes(event);
 
-  // 2. test selections and fill histograms  
-  bool nele_selection = nele_sel->passes(event);
-  bool nmu_selection = nmu_sel->passes(event);
-  bool njet_selection = njet_sel->passes(event);
-  bool twodcut_selection = twodcut->passes(event);
-  bool htlepcut_selection = htlepcut->passes(event);
-  bool metcut_selection = metcut->passes(event);
-  bool ntopjet_selection = ntopjet_sel->passes(event);
-  bool cmstoptagoverlap_selection = cmstoptagoverlap_sel->passes(event);
-  
+///  if(nele_selection && nmu_selection && njet_selection && twodcut_selection && htlepcut_selection && metcut_selection && ntopjet_selection && topjetoverlap_selection){
+///
+///    for(auto & m : recomodules) m->process(event);
+///
+///    h_event->fill(event);
+///    h_jet->fill(event);
+///    h_ele->fill(event);
+///    h_mu->fill(event);
+///    h_tau->fill(event);
+///    h_topjet->fill(event);
+///    h_hyphists->fill(event);
+///  }
 
-  //if(nele_selection && nmu_selection && njet_selection && twodcut_selection && htlepcut_selection && metcut_selection && ntopjet_selection && cmstoptagoverlap_selection){
-if(nele_selection && nmu_selection && njet_selection && twodcut_selection && htlepcut_selection && metcut_selection && ntopjet_selection && cmstoptagoverlap_selection){
-    for(auto & m : recomodules){
-        m->process(event);
-      }
-
-    h_event->fill(event);
-    h_jet->fill(event);
-    h_ele->fill(event);
-    h_mu->fill(event);
-    h_tau->fill(event);
-    h_topjet->fill(event);
-    h_hyphists->fill(event);
-  }
-
-  // 3. decide whether or not to keep the current event in the output:
-  return  nele_selection && nmu_selection && njet_selection && twodcut_selection && htlepcut_selection && metcut_selection && ntopjet_selection && cmstoptagoverlap_selection;
-
+  return pass_lepN && pass_jet1 && pass_jet2 && pass_twodcut && pass_htlep && pass_met && pass_topjetN && pass_topjetoverlap;
 }
-  
-  // as we want to run the ExampleCycleNew directly with AnalysisModuleRunner,
-  // make sure the ZprimeSelectionModule is found by class name. This is ensured by this macro:
-  UHH2_REGISTER_ANALYSIS_MODULE(ZprimeSelectionModule)
-  
 
+UHH2_REGISTER_ANALYSIS_MODULE(ZprimeSelectionModule)
