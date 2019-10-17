1c3284427c488e5a24e0445b53fbdeee69b6b109
Ksenia de Leo <ksenia.de.leo@desy.de>
2019-10-17 11:07:25 +0200

    update 2017 analysis QCD_HT samples

--- macros/CreateXML_for_Analysis_afterPreselection_2017.sh
+++ macros/CreateXML_for_Analysis_afterPreselection_2017.sh
@@ -2,14 +2,16 @@
 #where UHH2 code installed
 pathGL_code=/nfs/dust/cms/user/deleokse/analysis/CMSSW_10_2_10/src/UHH2/
 #where (NOT MERGED) trees after preselection stored
-path_data=/nfs/dust/cms/user/deleokse/analysis/ZprimeSemileptonic/Preselection/Output_2017/workdir_Zprime_PreSelec_2017/uhh2.AnalysisModuleRunner. 
+path_data=/nfs/dust/cms/user/deleokse/analysis/ZprimeSemileptonic/Preselection_QCD_HT/Output_2017/workdir_Zprime_PreSelec_2017/uhh2.AnalysisModuleRunner. 
 
-mkdir $pathGL_code/ZprimeSemiLeptonic/data/Skimming_datasets_20190928
-cd $pathGL_code/ZprimeSemiLeptonic/data/Skimming_datasets_20190928
+mkdir $pathGL_code/ZprimeSemiLeptonic/data/Skimming_datasets_20191016
+cd $pathGL_code/ZprimeSemiLeptonic/data/Skimming_datasets_20191016
 
 
 # #MC
-for sample_name in TTToSemiLeptonic_2017 TTToHadronic_2017 TTTo2L2Nu_2017 WW_2017 WZ_2017 ZZ_2017 ST_t-channel_antitop_2017 ST_t-channel_top_2017 ST_tW_antitop_5f_inclusiveDecays_2017 ST_tW_top_5f_inclusiveDecays_2017 QCD_Pt-15to30_2017 QCD_Pt-30to50_2017 QCD_Pt-50to80_2017 QCD_Pt-80to120_2017 QCD_Pt-120to170_2017 QCD_Pt-170to300_2017 QCD_Pt-300to470_2017 QCD_Pt-470to600_2017 QCD_Pt-600to800_2017 QCD_Pt-800to1000_2017 QCD_Pt-1000to1400_2017 QCD_Pt-1400to1800_2017 QCD_Pt-1800to2400_2017 QCD_Pt-2400to3200_2017 QCD_Pt-3200toInf_2017 WJetsToLNu_HT-70To100_2017 WJetsToLNu_HT-100To200_2017 WJetsToLNu_HT-200To400_2017 WJetsToLNu_HT-400To600_2017 WJetsToLNu_HT-600To800_2017 WJetsToLNu_HT-800To1200_2017 WJetsToLNu_HT-1200To2500_2017 WJetsToLNu_HT-2500ToInf_2017 ZprimeToTTJet_M1000_2017  
+#for sample_name in TTToSemiLeptonic_2017 TTToHadronic_2017 TTTo2L2Nu_2017 WW_2017 WZ_2017 ZZ_2017 ST_t-channel_antitop_2017 ST_t-channel_top_2017 ST_tW_antitop_5f_inclusiveDecays_2017 ST_tW_top_5f_inclusiveDecays_2017 QCD_Pt-15to30_2017 QCD_Pt-30to50_2017 QCD_Pt-50to80_2017 QCD_Pt-80to120_2017 QCD_Pt-120to170_2017 QCD_Pt-170to300_2017 QCD_Pt-300to470_2017 QCD_Pt-470to600_2017 QCD_Pt-600to800_2017 QCD_Pt-800to1000_2017 QCD_Pt-1000to1400_2017 QCD_Pt-1400to1800_2017 QCD_Pt-1800to2400_2017 QCD_Pt-2400to3200_2017 QCD_Pt-3200toInf_2017 WJetsToLNu_HT-70To100_2017 WJetsToLNu_HT-100To200_2017 WJetsToLNu_HT-200To400_2017 WJetsToLNu_HT-400To600_2017 WJetsToLNu_HT-600To800_2017 WJetsToLNu_HT-800To1200_2017 WJetsToLNu_HT-1200To2500_2017 WJetsToLNu_HT-2500ToInf_2017 ZprimeToTTJet_M1000_2017  
+
+for sample_name in TTToSemiLeptonic_2017 TTToHadronic_2017 TTTo2L2Nu_2017 WW_2017 WZ_2017 ZZ_2017 ST_t-channel_antitop_2017 ST_t-channel_top_2017 ST_tW_antitop_5f_inclusiveDecays_2017 ST_tW_top_5f_inclusiveDecays_2017 QCD_HT100to200_2017 QCD_HT200to300_2017 QCD_HT300to500_2017 QCD_HT500to700_2017 QCD_HT700to1000_2017 QCD_HT1000to1500_2017 QCD_HT1500to2000_2017 QCD_HT2000toInf_2017 WJetsToLNu_HT-70To100_2017 WJetsToLNu_HT-100To200_2017 WJetsToLNu_HT-200To400_2017 WJetsToLNu_HT-400To600_2017 WJetsToLNu_HT-600To800_2017 WJetsToLNu_HT-800To1200_2017 WJetsToLNu_HT-1200To2500_2017 WJetsToLNu_HT-2500ToInf_2017 ZprimeToTTJet_M1000_2017  
 
 do
     echo $sample_name
