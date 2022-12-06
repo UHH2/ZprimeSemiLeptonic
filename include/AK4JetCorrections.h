c653d098bc138a7020ea2f794bbb47b6096b5118
Ksenia de Leo <ksenia.de.leo@desy.de>
2022-12-06 14:06:11 +0100

    add JEC/JER in AnalysisModule to obtain up/down variations

---
+++ include/AK4JetCorrections.h
@@ -0,0 +1,39 @@
+#pragma once
+#include "UHH2/core/include/AnalysisModule.h"
+#include "UHH2/core/include/Selection.h"
+#include "UHH2/core/include/Utils.h"
+
+#include "UHH2/common/include/ObjectIdUtils.h"
+#include "UHH2/common/include/NSelections.h"
+#include "UHH2/common/include/JetIds.h"
+#include "UHH2/common/include/JetCorrections.h"
+#include "UHH2/common/include/CleaningModules.h"
+#include "UHH2/common/include/YearRunSwitchers.h"
+
+class AK4JetCorrections: public uhh2::AnalysisModule {
+public:
+    AK4JetCorrections();
+
+    virtual bool process(uhh2::Event & event) override;
+    void init(uhh2::Context & ctx);
+
+private:
+    void fail_if_init() const;
+
+    std::unique_ptr<YearSwitcher> jet_corrector_MC;
+    std::shared_ptr<RunSwitcher> jec_switcher_UL16preVFP, jec_switcher_UL16postVFP, jec_switcher_UL17, jec_switcher_UL18;
+    std::unique_ptr<GenericJetResolutionSmearer> jet_resolution_smearer;
+
+    bool is_mc, is_data;
+    bool init_done = false;
+
+    Year year;
+
+    // Parameters for JEC & JLC sets
+    std::string jec_tag_UL16preVFP, jec_ver_UL16preVFP;
+    std::string jec_tag_UL16postVFP, jec_ver_UL16postVFP;
+    std::string jec_tag_UL17, jec_ver_UL17;
+    std::string jec_tag_UL18, jec_ver_UL18;
+    std::string jec_jet_coll;
+};
+
