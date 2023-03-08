505f3649192df0037e41c82db6a9943b7f78c6ac
Henrik Jabusch <henrik.jabusch@uni-hamburg.de>
2023-03-08 13:08:07 +0100

    update plotter

---
+++ plotter/constants_mtt.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python2
+
+from collections import OrderedDict
+
+_VARIABLES = OrderedDict([
+    ('M_Zprime_ak4_rebin6', {
+        'name': 'M_Zprime_ak4_rebin6',
+        'x_axis_title': 'm(t#bar{t})',
+        'x_axis_unit': 'GeV'
+    }),
+])
