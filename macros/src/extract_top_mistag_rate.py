776bec31375d70a7dec91a263098c603eac7da4e
jabuschh <henrik.jabusch@uni-hamburg.de>
2023-07-07 14:27:24 +0200

    switch to top mistag SFs per year

--- macros/src/extract_top_mistag_rate.py
+++ macros/src/extract_top_mistag_rate.py
@@ -103,44 +103,20 @@ processes = OrderedDict([
 
 output = OrderedDict([
     ("UL16preVFP", {
-        "electron": {
-            "scale_factor": 0.0,
-            "error": 0.0
-        },
-        "muon": {
-            "scale_factor": 0.0,
-            "error": 0.0
-        }
+        "scale_factor": 0.0,
+        "error": 0.0
     }),
     ("UL16postVFP", {
-        "electron": {
-            "scale_factor": 0.0,
-            "error": 0.0
-        },
-        "muon": {
-            "scale_factor": 0.0,
-            "error": 0.0
-        }
+        "scale_factor": 0.0,
+        "error": 0.0
     }),
     ("UL17", {
-        "electron": {
-            "scale_factor": 0.0,
-            "error": 0.0
-        },
-        "muon": {
-            "scale_factor": 0.0,
-            "error": 0.0
-        }
+        "scale_factor": 0.0,
+        "error": 0.0
     }),
     ("UL18", {
-        "electron": {
-            "scale_factor": 0.0,
-            "error": 0.0
-        },
-        "muon": {
-            "scale_factor": 0.0,
-            "error": 0.0
-        }
+        "scale_factor": 0.0,
+        "error": 0.0
     })
 ])
 
@@ -150,16 +126,20 @@ output = OrderedDict([
 
 
 for year in years:
-    # print(year)
+    print(year)
+    el_SF = 0.0
+    el_SF_err = 0.0
+    mu_SF = 0.0
+    mu_SF_err = 0.0
 
     for channel in channels:
-        # print(channel)
+        print(channel)
 
         for key, process in processes.items():
             process_name = process.get("process_name")
             sample_name = process.get("sample_name")
             hist_name = process.get("hist_name")
-            # print(process_name)
+            print(process_name)
 
             file_path = path_base + year + "/Mistag_rate/" + channel + "/uhh2.AnalysisModuleRunner."
             if sample_name == "DATA":
@@ -183,25 +163,38 @@ for year in years:
             processes[process_name]["n_err"] = n_err
             processes[process_name]["n_tagged"] = n_tagged
             processes[process_name]["n_tagged_err"] = n_tagged_err
+            print("       events: " + str(n) + " +/- " + str(n_err))
+            print("tagged events: " + str(n_tagged) + " +/- " + str(n_tagged_err))
 
         # efficiency in DATA
         efficiency_data = (processes["DATA"]["n_tagged"] - processes["TTbar_t"]["n_tagged"] - processes["ST_t"]["n_tagged"]) / (processes["DATA"]["n"] - processes["TTbar_t"]["n"] - processes["ST_t"]["n"])
         efficiency_err_data = np.sqrt((processes["DATA"]["n_tagged_err"]**2 + processes["TTbar_t"]["n_tagged_err"]**2 + processes["ST_t"]["n_tagged_err"]**2) / (processes["DATA"]["n"] - processes["TTbar_t"]["n"] - processes["ST_t"]["n"])**2  +
         ((processes["DATA"]["n_err"]**2 + processes["TTbar_t"]["n_err"]**2 + processes["ST_t"]["n_err"]**2)) * (processes["DATA"]["n_tagged"] - processes["TTbar_t"]["n_tagged"] - processes["ST_t"]["n_tagged"])**2 / (processes["DATA"]["n"] - processes["TTbar_t"]["n"] - processes["ST_t"]["n"])**4)
-        # print("efficiency data: " + str(efficiency_data) + " +/- " + str(efficiency_err_data))
+        print("efficiency data: " + str(efficiency_data) + " +/- " + str(efficiency_err_data))
 
         # efficiency in MC
         efficiency_mc = (processes["WJets"]["n_tagged"] + processes["TTbar_l"]["n_tagged"] + processes["ST_l"]["n_tagged"] + processes["DY"]["n_tagged"] + processes["QCD"]["n_tagged"]) / (processes["WJets"]["n"] + processes["TTbar_l"]["n"] + processes["ST_l"]["n"] + processes["DY"]["n"] + processes["QCD"]["n"])
         efficiency_err_mc = np.sqrt((processes["WJets"]["n_tagged_err"]**2 + processes["TTbar_l"]["n_tagged_err"]**2 + processes["ST_l"]["n_tagged_err"]**2 + processes["DY"]["n_tagged_err"]**2 + processes["QCD"]["n_tagged_err"]**2) / (processes["WJets"]["n"] + processes["TTbar_l"]["n"] + processes["ST_l"]["n"] + processes["DY"]["n"] + processes["QCD"]["n"])**2 +
         (processes["WJets"]["n_err"]**2 + processes["TTbar_l"]["n_err"]**2 + processes["ST_l"]["n_err"]**2 + processes["DY"]["n_err"]**2 + processes["QCD"]["n_err"]**2) * (processes["WJets"]["n_tagged"] + processes["TTbar_l"]["n_tagged"] + processes["ST_l"]["n_tagged"] + processes["DY"]["n_tagged"] + processes["QCD"]["n_tagged"])**2 / (processes["WJets"]["n"] + processes["TTbar_l"]["n"] + processes["ST_l"]["n"] + processes["DY"]["n"] + processes["QCD"]["n"])**4)
-        # print("  efficiency mc: " + str(efficiency_mc) + " +/- " + str(efficiency_err_mc))
+        print("  efficiency mc: " + str(efficiency_mc) + " +/- " + str(efficiency_err_mc))
 
         scale_factor = efficiency_data / efficiency_mc
         scale_factor_err = np.sqrt((efficiency_err_data / efficiency_mc)**2 + efficiency_err_mc**2 * efficiency_data**2 / efficiency_mc**4)
-        # print("   scale factor: " +  str(scale_factor) + " +/- " + str(scale_factor_err))
+        print("   scale factor: " +  str(scale_factor) + " +/- " + str(scale_factor_err))
+        if channel == "electron":
+            el_SF = scale_factor
+            el_SF_err = scale_factor_err
+        else:
+            mu_SF = scale_factor
+            mu_SF_err = scale_factor_err
 
-        output[year][channel]["scale_factor"] = scale_factor
-        output[year][channel]["error"] = scale_factor_err
+    SF = (el_SF + mu_SF) / 2.
+    SF_err = np.sqrt(el_SF_err**2 + mu_SF_err**2)
+    print("----------")
+    print("scale factor (" + year + "): " + str(SF) + " +/- " + str(SF_err))
+
+    output[year]["scale_factor"] = SF
+    output[year]["error"] = SF_err
 
 output_json = json.dumps(output, indent=4)
 print(output_json)
